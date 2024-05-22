da085e0f4fb1f1350857f792870d9303e0d9bad3
Antti Pulkkinen <apulkki78@welho.com>
2024-05-22 17:23:22 +0300

    docker file and test.js updated

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const express = require("express");
 const os = require("os");
 const path = require("path");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
     res.send("<h1>Hello World!</h1>");
